010744478cdb543c1cc4263e06a105b0f0357950
O1Dii <oldi730051@mail.ru>
2019-12-20 20:29:44 +0300

    Fixed prices array type, changed actiovations

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 squareFeet = np.array(range(1, 9000), dtype="int16")
-prices = np.array([i ** 2 / 1000000 for i in squareFeet], dtype="int32")
+prices = np.array([i ** 2 / 1000000 for i in squareFeet], dtype="float16")
 
 model = keras.Sequential()
-model.add(keras.layers.Dense(20, input_dim=1, activation='linear'))
-model.add(keras.layers.Dense(200, input_dim=1, activation='elu'))
-model.add(keras.layers.Dense(200, input_dim=1, activation='elu'))
+model.add(keras.layers.Dense(20, input_dim=1, activation='relu'))
+model.add(keras.layers.Dense(200, activation='elu'))
+model.add(keras.layers.Dense(200, activation='elu'))
 model.add(keras.layers.Dense(1))
 model.compile(optimizer='adam', loss='mse')
 
