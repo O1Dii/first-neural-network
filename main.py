90d0b280650f93bdf03eb4dc003dd650d65efe25
O1Dii <oldi730051@mail.ru>
2019-12-20 20:19:49 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+squareFeet = np.array(range(1, 9000), dtype="int16")
+prices = np.array([i ** 2 / 1000000 for i in squareFeet], dtype="int32")
+
+model = keras.Sequential()
+model.add(keras.layers.Dense(20, input_dim=1, activation='linear'))
+model.add(keras.layers.Dense(200, input_dim=1, activation='elu'))
+model.add(keras.layers.Dense(200, input_dim=1, activation='elu'))
+model.add(keras.layers.Dense(1))
+model.compile(optimizer='adam', loss='mse')
+
+model.fit(squareFeet, prices, epochs=30, batch_size=10)
+
+price_predict = model.predict(squareFeet)
+
+plt.plot(squareFeet, prices, label="initial", color="red")
+plt.plot(squareFeet, price_predict, label="prediction", color="blue")
+plt.legend()
+
+plt.show()
